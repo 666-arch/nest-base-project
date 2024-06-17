cd0b7117467e93c58a785042b206871069db9fee
666-arch <hc18174419502@163.com>
2024-06-17 22:20:14 +0800

    push token

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Inject, Post, Res } from '@nestjs/common';
 import { UserService } from './user.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/reagister.dto';
 import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Controller('user')
 export class UserController {
@@ -10,15 +11,23 @@ export class UserController {
   @Inject(JwtService)
   private jwtService: JwtService;
   @Post('login')
-  async login(@Body() user: LoginDto) {
+  async login(
+    @Body() user: LoginDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
     const foundUser = await this.userService.login(user);
     if (foundUser) {
+      //将登陆成功后的用户信息加载到jwt，生产token，设置header头，返回到客户端
       const token = await this.jwtService.signAsync({
         user: {
           id: foundUser.id,
           username: foundUser.username,
         },
       });
+      res.setHeader('token', token);
+      return 'login success';
+    } else {
+      return 'login failed';
     }
   }
   @Post('register')
